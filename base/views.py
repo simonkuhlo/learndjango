1b564375965f73653e46c08282aba0d4129a1c5c
simonkuhlo <siga241@gmail.com>
2023-10-31 21:57:33 +0100

    passing and rendering context (55)

--- base/views.py
+++ base/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+rooms = [
+    {id : 1, "name" : "Hallo"},
+    {id : 2, "name" : "Hallo2"},
+    {id : 3, "name" : "Hallo3"},
+    {id : 4, "name" : "Hallo4"},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {"rooms":rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
