628d7860c01fbbae848c346931c05ac8aa0f8aa1
simon <siga241@gmail.com>
2023-11-07 20:11:08 -0500

    key manager

--- key_manager/views.py
+++ key_manager/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Key
 
 # Create your views here.
+def test(request):
+    keys = Key.objects.all()
+    ctx = {"keys" : keys}
+    return render(request, 'key_manager/test.html', ctx)
