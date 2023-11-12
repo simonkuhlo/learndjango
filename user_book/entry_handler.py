0a843a46550cfb0e277941300cc82c8fdc4d55be
simon <siga241@gmail.com>
2023-11-12 10:40:39 -0500

    changed entry layout

--- user_book/entry_handler.py
+++ user_book/entry_handler.py
@@ -3,7 +3,7 @@ from . import models
 
 def entry_to_dict(etnryid):
     object = models.Entry.objects.get(id=etnryid)
-    answers = models.Answer.objects.filter(entry=etnryid)
+    answers = models.Answer.objects.filter(entry=etnryid).order_by('question__sort_id')
     author = models.people.Person.objects.get(id=object.author.id)
     interview_dict = {}
     for answer in answers:
