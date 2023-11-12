02ef95211ef920ae0a7cde8275faeb777da72d8b
simonkuhlo <siga241@gmail.com>
2023-11-11 22:04:27 -0500

    create entry dict by id

--- user_book/entry_handler.py
+++ user_book/entry_handler.py
@@ -4,4 +4,14 @@ from . import models
 def entry_to_dict(etnryid):
     object = models.Entry.objects.get(id=etnryid)
     answers = models.Answer.objects.filter(entry=etnryid)
-    print(answers[0].entry)
+    interview_dict = {}
+    for answer in answers:
+        question_model = models.Question.objects.get(id=answer.question)
+        interview_dict.update({question_model.question_value : answer.answer_value})
+    
+    entry_dict = {
+        "about_me" : "TODO",
+        "profile_picture" : "TODO",
+        "interview" : interview_dict
+    }
+    print(entry_dict)
