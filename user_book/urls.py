a3a7d0e4bc781f87556f37d56cf1bf4d3a6bed8c
simonkuhlo <siga241@gmail.com>
2023-11-18 13:19:15 -0500

    admin panel fix

--- user_book/urls.py
+++ user_book/urls.py
@@ -4,8 +4,8 @@ from . import views_admin
 
 urlpatterns = [
     path('', views.home, name = "home"),
-    path('entry/id:<str:pk>/', views.entry, name = "person"),
+    path('entry/id:<str:pk>/', views.entry, name = "interview_viewer"),
     path('new_entry/interview/id:<str:pk>', views.interview, name = "person"),
-    path('home', views.home, name = "home"),
-    path('admin', views_admin.interview_manager.as_view(), name = "admin"),
+    path('home/', views.home, name = "home"),
+    path('admin/', views_admin.interview_manager.as_view(), name = "admin"),
 ]
