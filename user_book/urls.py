db7de3e52209c646cd17633576c8a9b88b258031
simonkuhlo <siga241@gmail.com>
2023-11-18 05:10:31 -0500

    improved interview viewer

--- user_book/urls.py
+++ user_book/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from . import views_admin
 
 urlpatterns = [
     path('', views.home, name = "home"),
     path('entry/id:<str:pk>/', views.entry, name = "person"),
     path('new_entry/interview/id:<str:pk>', views.interview, name = "person"),
+    path('interview_viewer/id:<str:pk>', views_admin.interview, name = "interview_viewer"),
 ]
