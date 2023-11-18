718515c06a9ef04fa7a781d17ecff82c156b2888
simonkuhlo <siga241@gmail.com>
2023-11-18 15:51:14 -0500

    cleanup

--- user_book/urls.py
+++ user_book/urls.py
@@ -5,6 +5,7 @@ from . import views_admin
 urlpatterns = [
     path('', views.home, name = "home"),
     path('entry/id:<str:pk>/', views.entry, name = "entry"),
+    
     path('new_entry/interview:<str:pk>', views.interview, name = "interview"),
     path('admin/edit_question/id:<str:pk>', views_admin.edit_question, name = "edit_question"),
     path('admin/edit_question/success/', views_admin.edit_question_success, name = "edit_question"),
